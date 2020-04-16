41e3667d4ba47e009cfb2a6126c34daca289b907
John Cosio <mail@jcos.io>
2020-04-17 00:19:47 +0800

    Remove console log

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-console.log(process.env.NODE_ENV);
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
